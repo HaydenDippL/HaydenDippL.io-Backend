ce93c488ef64d34ed92eeeac3b218a0e320b302d
Hayden Dippel <hdippel@wisc.edu>
2025-02-19 22:57:15 -0600

    cannot have / at end of cors allow path

--- backend/settings.py
+++ backend/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
-    'https://haydendippl.github.io/',
+    'https://haydendippl.github.io',
 ]
 
 ROOT_URLCONF = 'backend.urls'
